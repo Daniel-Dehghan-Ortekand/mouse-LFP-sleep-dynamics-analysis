deeb0424b39bc2f8b1b446e7d0a56eb232656fa2
Daniel Dehghan Ortekand <daniel.dehghan.ortekand@gmail.com>
2026-09-05 23:22:59 +0330

    Create data information.m

---
+++ Mouse12-120806/data information.m
@@ -0,0 +1,15 @@
+%Recording from the anterior thalamus, hippocampus (CA1) and mPFC, left hemisphere. 
+%Anterior Thalamus: Electrode groups: 1-8. 8 shank probe, Neuronexus Buz64 design.
+ %Shank #1 is the most lateral,
+ %#8 the most medial Probe is perpendicular to the midline (coronal plane), 
+%tilted by 15 degrees (tips pointed toward midline), mounted on a movable drive. 
+%Insertion coordinates: AP -0.7mm, ML 0.5mm Approx depth from surface: 2.97mm medial Prefrontal (Prelimbic): 
+%Electrode Groups: 9-12 4 tetrodes Insertion coordinates: AP 1.78 - 1.94mm, ML 0.5mm, depth: 1.7mm Hippocampus: 
+%Electrode group: 14. 5 tungsten wires (fifty micrometer in diamater), 
+%one (at least) is in the pyramidal layer of CA1 as shown by the presence of Sharp-Wave/Ripples. 
+%Insertion coordinates: AP -2.2mm, ML 1.3mm, depth: 1.7mm
+
+
+
+
+
